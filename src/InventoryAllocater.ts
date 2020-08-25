fac108039b8a7f1ef23dc3ac758cd0deb2a0751a
Mikolaj Figurski <meek.f80@gmail.com>
2020-08-25 12:52:41 -0400

    Implement InventoryAllocater.allocate

--- src/InventoryAllocater.ts
+++ src/InventoryAllocater.ts
@@ -1,17 +1,33 @@
 import Inventory from './types/Inventory'
 import Warehouse from './types/Warehouse'
 import AllocateReturn from './types/InventoryAllocater/AllocateReturn'
-import { map } from 'lodash'
+import { forEach } from 'lodash'
 
-var data: [Warehouse] = [{ name: 'owd', inventory: { apple: 1 } }]
-var order: Inventory = { banana: 5, orange: 10 }
 
 class InventoryAllocater {
-    static allocate(order: Inventory, inventoryDistribution: Array<Warehouse>): AllocateReturn {
-        console.log('Allocating')
 
-        return []
+    static allocate(order: Inventory, inventoryDistribution: Array<Warehouse>): AllocateReturn {
+        order = { ...order } // clone
+        let fill: any = {}
+
+        forEach(order, (count, item) => { // for each ordered item...
+
+            inventoryDistribution.some((w, i) => {  // check warehouses in order
+                if (!w.inventory[item]) return
+                if (!fill[w.name]) fill[w.name] = {}
+
+                let amount = Math.min(count, w.inventory[item])
+                fill[w.name][item] = amount
+                count -= amount
+                if (count == 0) return false // no need to keep checking this item
+            })
+
+        })
+
+
+        return Object.keys(fill).sort().map((key) => ({ [key]: fill[key] }))
     }
+
 }
 
 
